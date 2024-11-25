3e66d064228e8c8bb43026152cdbe15b83164d58
524hhhhhh <srose1225@naver.com>
2024-11-25 17:35:18 +0900

    test

--- js/buttonEvent.js
+++ js/buttonEvent.js
@@ -1,9 +1,3 @@
 document.querySelector('#moveButton').addEventListener('click', function () {
 	window.location.href = 'cat.html';
 });
-
-document.querySelectorAll('.likeButton').forEach(function (button) {
-	button.addEventListener('click', function () {
-		alert('먕');
-	});
-});
